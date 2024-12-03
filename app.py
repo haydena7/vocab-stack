7b536f8487eaf6dbb37b9e4a06cec70bb61230bb
haydenanderson <107340527+haydena7@users.noreply.github.com>
2024-12-03 13:24:45 -0600

    Refactor to single-file structure

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
+from typing import Optional
+
 from starlette.applications import Starlette
 from starlette.routing import Route, Mount
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
+from sqlmodel import Field, Session, SQLModel, or_, create_engine, select, col
+
+
+class Vocab(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    word: str = Field(index=True)
+    context: Optional[str] = None
+    source: Optional[str] = None
+    freq: Optional[float] = Field(default=None, index=True)  # ¿data type correct?
+
+
+sqlite_fname = 'database.db'
+sqlite_url = f'sqlite:///{sqlite_fname}'
+
+engine = create_engine(sqlite_url, echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
 templates = Jinja2Templates(directory='templates')
 
-async def homepage(request):
-    return templates.TemplateResponse(request, 'index.html')
+
+async def homepage(request: Request):
+    return RedirectResponse(url='/vocabs', status_code=301)
+
+
+async def vocabs(request: Request):
+    search = request.query_params.get('q')
+    with Session(engine) as session:
+        if search is not None:
+            vocabs_set = session.exec(select(Vocab).where(or_(
+                col(Vocab.word).icontains(search),
+                col(Vocab.context).icontains(search),
+                col(Vocab.source).icontains(search),
+            ))).all()
+        else:
+            vocabs_set = session.exec(select(Vocab)).all()
+    return templates.TemplateResponse(request, 'index.html', {'vocabs': vocabs_set})
+
 
 routes = [
     Route('/', homepage),
-    Mount('/static', StaticFiles(directory='static'), name='static')
+    Route('/vocabs', vocabs),
+    Mount('/static', StaticFiles(directory='static'), name='static'),
 ]
 
-app = Starlette(debug=True, routes=routes)
+on_startup = [
+    create_db_and_tables,
+]
+
+app = Starlette(debug=True, routes=routes, on_startup=on_startup)
